a89586dc67870937d3732236115d4615ed0c2b63
yasarcandamli <yasarcandamli@gmail.com>
2025-04-17 23:09:04 +0300

    Bucket name & Cluster name updated

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "vprofileactions3301"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 
   required_version = "~> 1.6.3"
